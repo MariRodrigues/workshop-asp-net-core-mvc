b4e224db5f92df992d77ca867fb01c6a1bac3211
Mariana Souza <mari_rodrigue@hotmail.com>
2019-01-23 16:40:07 -0200

    Other entities and second migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -8,11 +8,18 @@ namespace SalesWebMvc.Models
 {
     public class SalesWebMvcContext : DbContext
     {
+        //Para que as classes sejam reconhecidas pelo Entitie Framework, elas devem ser adicionadas
+        //como DbSet's, nesta classe! 
         public SalesWebMvcContext (DbContextOptions<SalesWebMvcContext> options)
             : base(options)
         {
         }
 
-        public DbSet<SalesWebMvc.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+        
+
+
     }
 }
--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -3,11 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+        // Utiliza-se o ICollection pois é a lista mais genérica. 
+        // Faz a associação de Departamento, com o vendedor (seller)
+        // Pois, cada departamento terá vários vendedores. 
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller (Seller seller)
+        { //Adiciona um vendedor no departamento
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales (DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+            // Para todos os vendedores do departamento, de Seller, chama o método TotalSales 
+            // mandando como argumento as datas desejadas, e soma os resultados
+        }
+
     }
 }
