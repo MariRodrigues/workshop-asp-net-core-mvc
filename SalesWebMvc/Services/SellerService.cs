33c84313adfa70a59fee703bc4541259e4ac3ce9
Mariana Souza <mari_rodrigue@hotmail.com>
2019-01-23 18:09:53 -0200

    SellerService and basic FindAll

---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _context; // Dependência para o DbContext
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+            // Acessa a fonte de dados da tabela de vendedores, e converter para uma lista
+            // Opração síncrona = Quando executada, a aplicação fica bloqueada até que termine a operação.
+        }
+
+
+    }
+}
